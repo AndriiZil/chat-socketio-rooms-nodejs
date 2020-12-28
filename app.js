4c60d692470726c876d751cb84ca7c2e4eaf41d3
Andrii Zilnyk <andrii.zilnyk@gmail.com>
2020-12-28 18:41:27 +0200

    added chat

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const socket = require('socket.io')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+const port = process.env.PORT || 3000;
+
+//Render Index page
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+//Get username and roomname from form and pass it to room
+app.post('/room', (req, res) => {
+    const roomname = req.body.roomname;
+    const username = req.body.username;
+    res.redirect(`/room?username=${username}&roomname=${roomname}`)
+});
+
+//Rooms
+app.get('/room', (req, res)=>{
+    res.render('room')
+});
+
+//Start Server
+const server = app.listen(port, () => {
+    console.log(`Server Running on ${port}`)
+});
+
+const io = socket(server);
+require('./utils/socket')(io);
